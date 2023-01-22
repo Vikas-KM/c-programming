b09b1ab40ecbf23f0dc753464453d2bf6b3ae2e5
Vikas KM <vikki.km@gmail.com>
2023-01-22 15:27:33 +0530

    declare variables

---
+++ dec_variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int myInt = 10;
+    double myDouble = 10.00;
+    char myChar = 'a';
+    string myString = "Hello";
+    bool myBool = true;
+
+    cout << myInt << endl;
+    cout << myDouble << endl;
+    cout << myChar << endl;
+    cout << myString << endl;
+    cout << myBool << endl;
+    cout << x << y << z << endl;
+    cout << p << q << r << endl;
+}
--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    // Prints hello world
     cout << "Hello World!" << endl;
     return 0;
 }
